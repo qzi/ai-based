6ecaa5ed67d9e3b29ecab5f68be228e9c6079d9b
Leon Qiu <leon.qiu@agilelog.org>
2023-05-02 21:41:25 +0800

    add nodejs sample of openai

---
+++ gpt-node/gpt-node.js
@@ -0,0 +1,12 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+const completion = await openai.createCompletion({
+  model: "text-davinci-003",
+  prompt: "Who will win the next eleciton in US?",
+});
+console.log(completion.data.choices[0].text);
